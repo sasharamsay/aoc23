2a3801cfbf172f03fb2649d2d38a46c04c4f36ff
Sasha Ramsay <schopelas@qualtrics.com>
2023-12-18 11:08:48 -0700

    stubbed in day 8, put day functions into a map

---
+++ day8.py
@@ -0,0 +1,12 @@
+def day_8(filename):
+    for line in open(filename, 'r').readlines():
+        print(line)
+
+    return "not done"
+
+
+def day_8_part_2(filename):
+    for line in open(filename, 'r').readlines():
+        print(line)
+
+    return "not done"
--- main.py
+++ main.py
@@ -5,27 +5,24 @@ from day4 import *
 from day5 import *
 from day6 import *
 from day7 import *
+from day8 import *
 
 
 def run(day, part):
     filename = "input/in" + str(day) + ".txt"
-    match day:
-        case 1:
-            print(day_1(filename)) if part == 1 else print(day_1_part_2(filename))
-        case 2:
-            print(day_2(filename)) if part == 1 else print(day_2_part_2(filename))
-        case 3:
-            print(day_3(filename)) if part == 1 else print(day_3_part_2(filename))
-        case 4:
-            print(day_4(filename)) if part == 1 else print(day_4_part_2(filename))
-        case 5:
-            print(day_5(filename)) if part == 1 else print(day_5_part_2(filename))
-        case 6:
-            print(day_6(filename)) if part == 1 else print(day_6_part_2(filename))
-        case 7:
-            print(day_7(filename)) if part == 1 else print(day_7_part_2(filename))
-        case _:
-            print("Day " + str(day) + " isn't done.")
+
+    day_map = {
+        1: (day_1, day_1_part_2),
+        2: (day_2, day_2_part_2),
+        3: (day_3, day_3_part_2),
+        4: (day_4, day_4_part_2),
+        5: (day_5, day_5_part_2),
+        6: (day_6, day_6_part_2),
+        7: (day_7, day_7_part_2),
+        8: (day_8, day_8_part_2),
+    }
+
+    print(day_map[day][part-1](filename))
 
 
 if __name__ == '__main__':
