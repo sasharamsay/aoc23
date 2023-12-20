28c5133c51cf09a7633e8f2b36819dc25840996a
Sasha Ramsay <schopelas@qualtrics.com>
2023-12-20 01:52:41 -0700

    one-liner ifs and better comments

--- day6.py
+++ day6.py
@@ -5,22 +5,17 @@ import re
 def get_range(race_time, distance_to_beat):
     # button_press_time * (race_time - button_press_time) = distance_traveled
     # x * (time - x) = dist
-
-    # so in a 7 millisecond race:
+    # press for 1ms in a 7ms race:
     # 1 * (7 - 1) = 6mm
 
-    # eww math
     # x * (time - x) = dist
     # (x * time) - x^2 = dist
     # (x * time) = x^2 + dist
     # y = x^2 - (x * time) + dist
 
     # quadratic formula:
-    # y = ax^2 + bx + c
-
-    # a = 1
-    # b = 0 - race_time
-    # c = distance to beat
+    # (-b +/- sqrt(b ^ 2) - (4 * a * c)) / 2 * a
+    # where ax^2 + bx + c
 
     a = 1
     b = 0 - race_time
@@ -29,15 +24,8 @@ def get_range(race_time, distance_to_beat):
     min_time_raw = ((0 - b) - (math.sqrt(pow(b, 2) - 4 * a * c))) / (2 * a)
     max_time_raw = ((0 - b) + (math.sqrt(pow(b, 2) - 4 * a * c))) / (2 * a)
 
-    if min_time_raw.is_integer():
-        min_time = min_time_raw + 1
-    else:
-        min_time = math.ceil(min_time_raw)
-
-    if max_time_raw.is_integer():
-        max_time = max_time_raw - 1
-    else:
-        max_time = math.floor(max_time_raw)
+    min_time = min_time_raw + 1 if min_time_raw.is_integer() else math.ceil(min_time_raw)
+    max_time = max_time_raw - 1 if max_time_raw.is_integer() else math.floor(max_time_raw)
 
     ways_to_win = 0
 
@@ -52,17 +40,11 @@ def day_6(filename):
 
     times_init = lines[0].split(':')[1]
     times = re.split("\s+", times_init)
-
-    for t in times:
-        if t == '':
-            times.remove(t)
+    times = [i for i in times if i != '']
 
     records_init = lines[1].split(':')[1]
     records = re.split("\s+", records_init)
-
-    for r in records:
-        if r == '':
-            records.remove(r)
+    records = [i for i in records if i != '']
 
     product = 1
 
@@ -81,11 +63,9 @@ def day_6_part_2(filename):
     time = record = ''
 
     for char in times_init:
-        if not char.isspace():
-            time += char
+        time += char if not char.isspace() else ''
 
     for char in records_init:
-        if not char.isspace():
-            record += char
+        record += char if not char.isspace() else ''
 
     return get_range(int(time), int(record))
